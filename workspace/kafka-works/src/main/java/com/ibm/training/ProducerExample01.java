54ba3aa1b2486475e4e163aa0c6f9eef0f827161
Naveen <adith.naveen@gmail.com>
2023-08-08 16:16:09 +0530

    after producer and kafka

--- workspace/kafka-works/src/main/java/com/ibm/training/ProducerExample01.java
+++ workspace/kafka-works/src/main/java/com/ibm/training/ProducerExample01.java
@@ -2,7 +2,9 @@ package com.ibm.training;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,26 +21,33 @@ public class ProducerExample01 {
 		
 		
 		// set all the properties then given it to kafka producer 
-		
+		// more configurations if needed can be loaded here 
 		Properties prop = new Properties(); 
-		prop.put("bootstrap.servers", host +":"+port);
+		prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host +":"+port);
 		prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
 		prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		
-		// more configurations if needed can be loaded here 
-		
+	
 		
 		// create producer 
+		KafkaProducer<String, String> producer = new KafkaProducer<>(prop); 
 		
 		// create a record 
 		
-		// attache the record to the producer  and send 
+		ProducerRecord<String, String> producerRecord = 
+				new ProducerRecord<String, String>("topic-partition-3", "hello from java1");  
+		
+		// attach the record to the producer  and send 
+		producer.send(producerRecord); 
+		
 		
 		// flush the producer to the the connection is closed 
+		producer.flush(); 
+		
 		
 		// finally closed 
+		producer.close(); 
 		
-		
+		logger.info("Record sent to kafka");
 		
 		
 	}
